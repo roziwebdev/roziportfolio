924793c28175a67976b78409dc7a0f2e4efa79ca
Rozi <145320304+roziwebdev@users.noreply.github.com>
2024-09-27 10:07:44 +0700

    Update ProjectCard.tsx

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -12,41 +12,48 @@ interface ProjectCardProps {
   img: string;
   type: string;
   title: string;
-    desc: string;
-    href: string;
+  desc: string;
+  href: string;
 }
 
-export function ProjectCard({ img, title, desc,type, href }: ProjectCardProps) {
+export function ProjectCard({ img, title, desc, type, href }: ProjectCardProps) {
   return (
-    <Card  className="my-5 w-full p-16 justify-between lg:flex-row bg-gradient-to-r from-white to-blue-50 rounded-r-none"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+    <Card className="my-5 w-full p-8 justify-between lg:flex-row bg-gradient-to-r from-white to-blue-50 rounded-r-none lg:items-center lg:flex lg:space-x-6">
       <CardHeader
-              shadow={false}
-              floated={false}
-              className="m-0  rounded-r-none"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}          >
+        shadow={false}
+        floated={false}
+        className="lg:w-1/3 min-w-[300px] lg:min-w-[400px] rounded-r-none"
+      >
         <a href={href} className="cursor-pointer" target="_blank">
-        <Image
-          width={600}
-          height={600}
-          src={img}
-          alt={title}
-          className="h-full w-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
-        />
+          <Image
+            width={600}
+            height={600}
+            src={img}
+            alt={title}
+            className="h-full w-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+          />
         </a>
       </CardHeader>
-      <CardBody  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} >
-        <Typography variant="paragraph" color="gray" className="text-sm mb-4 uppercase flex items-center gap-2"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-            <PencilSquareIcon className="h-5 w-5"/>
-          { type}
+      <CardBody className="lg:w-2/3 flex-grow">
+        <Typography
+          variant="paragraph"
+          color="gray"
+          className="text-sm mb-4 uppercase flex items-center gap-2"
+        >
+          <PencilSquareIcon className="h-5 w-5" />
+          {type}
         </Typography>
-        <Typography variant="h4" color="blue-gray" className="mb-2"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+        <Typography variant="h4" color="blue-gray" className="mb-2">
           {title}
         </Typography>
-        <Typography color="gray" className="mb-8 font-normal"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+        <Typography color="gray" className="mb-8 font-normal">
           {desc}
         </Typography>
         <a href={href} target="_blank" className="inline-block">
-            
-          <Button variant="text" className="flex items-center gap-2 text-blue-500"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+          <Button
+            variant="text"
+            className="flex items-center gap-2 text-blue-500"
+          >
             Learn More
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -65,7 +72,7 @@ export function ProjectCard({ img, title, desc,type, href }: ProjectCardProps) {
           </Button>
         </a>
       </CardBody>
-      </Card>
+    </Card>
   );
 }
 
