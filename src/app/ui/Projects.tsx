e721d7d1a5525d63cf316be73379880fff0b3ea8
Rozi <145320304+roziwebdev@users.noreply.github.com>
2024-09-29 18:58:34 +0700

    Update Projects.tsx deskripsi

--- src/app/ui/Projects.tsx
+++ src/app/ui/Projects.tsx
@@ -8,21 +8,21 @@ const PROJECTS = [
     type:"Laravel PHP",
     img: "/image/project/arjayasite.png",
     title: "System Management",
-    desc: "Proyek ini bertujuan untuk membangun sebuah sistem manajemen berbasis website yang komprehensif dan terintegrasi guna mendukung berbagai aspek operasional bisnis perusahaan. Sistem ini akan mencakup modul-modul penting seperti : Purchase Order, Product Decelopment, Sales, HRGA, Inventory dan masih banyak lagi.",
+    desc: "This project aims to develop a comprehensive and integrated web-based management system to support various aspects of the company's business operations.",
     href:"https://rozi.arjaya.site/",
   },
   {
     type:"Next JS",
     img: "/image/project/companyprofile.png",
     title: "Company Profile & E-Commerce",
-      desc: "Proyek ini bertujuan untuk membangun website company profile yang modern dan informatif, dilengkapi dengan fitur e-commerce untuk memudahkan transaksi jual-beli produk perusahaan secara online. ",
+      desc: "This project aims to build a modern and informative company profile website, equipped with e-commerce features to facilitate seamless online transactions for the company products.",
     href:"https://arjayaprint.vercel.app/"
   },
   {
     type:"Next JS",
     img: "/image/project/admindashboard.png",
     title: "Admin Dashboard E-Commerce",
-      desc: "Proyek ini bertujuan untuk membangun sistem admin berbasis website yang memungkinkan administrator mengelola produk dan pengguna dalam platform e-commerce. Sistem ini mencakup fitur-fitur penting sebagai berikut: Managemen Produk dan Pengguna, Dashboard Statistik dan Laporan, dan lain sebagainya",
+      desc: "This project aims to build a web-based admin system that allows administrators to manage products and users within the e-commerce platform.",
     href:"https://admindashboard-rozi.vercel.app/"
   }
 ];
@@ -37,9 +37,7 @@ export function Projects() {
         <Typography
                   variant="lead"
                   className="mx-auto w-full px-4 font-normal !text-gray-500 lg:w-6/12"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}        >
-          Whether you have a mobile app idea that needs to come to life or a
-          website that requires a facelift, I&apos;m here to turn your digital
-          dreams into reality.
+          Several projects I’ve worked on prioritize efficient functionality and flexible design, ensuring optimal solutions for your digital needs.
         </Typography>
       </div>
         {PROJECTS.map((props, idx) => (
